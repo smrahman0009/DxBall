f51634afdbebd18843a89bd03f2f72cd02722c0a
Mushfikur Rahman <35715315+smrahman0009@users.noreply.github.com>
2018-08-28 14:53:17 +0600

    Ball not colide with bar issue fixed

--- app/src/main/java/com/example/musfiq/dxball/Bar.java
+++ app/src/main/java/com/example/musfiq/dxball/Bar.java
@@ -33,17 +33,17 @@ public class Bar {
     // in the screen width and height
     public Bar(int screenX, int screenY){
         // 130 pixels wide and 20 pixels high
-        length = 260;
-        height = 40;
+        length = 130;
+        height = 20;
 
         // Start paddle in roughly the sceen centre
         x = screenX / 2;
-        y = screenY - 40;
+        y = screenY - 20;
 
         rect = new RectF(x, y, x + length, y + height);
 
         // How fast is the paddle in pixels per second
-        paddleSpeed = 450;
+        paddleSpeed = 350;
     }
 
     // This is a getter method to make the rectangle that
