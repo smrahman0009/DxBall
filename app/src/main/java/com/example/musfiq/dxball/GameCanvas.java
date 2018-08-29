23176e62227dc2e9ed91af755f9cb7559b171f29
Mushfikur Rahman <35715315+smrahman0009@users.noreply.github.com>
2018-08-29 11:31:44 +0600

    Add another activity

---
+++ app/src/main/java/com/example/musfiq/dxball/GameCanvas.java
@@ -0,0 +1,14 @@
+package com.example.musfiq.dxball;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameCanvas extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_canvas);
+    }
+}
