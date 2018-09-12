fffcad30c6707a7d02184caed702900e23205fa4
Mushfikur Rahman <35715315+smrahman0009@users.noreply.github.com>
2018-09-13 02:56:14 +0600

    add sound

--- app/src/main/java/com/example/musfiq/dxball/GameCanvas.java
+++ app/src/main/java/com/example/musfiq/dxball/GameCanvas.java
@@ -1,18 +1,25 @@
+
 package com.example.musfiq.dxball;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.RectF;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import java.io.IOException;
 
 public class GameCanvas extends Activity {
 
@@ -31,7 +38,7 @@ public class GameCanvas extends Activity {
         private boolean GAME_LEVEL_ONE;
         private boolean GAME_LEVEL_TWO;
         private boolean GAME_LEVEL_THREE;
-        boolean firstTime;
+        //    boolean firstTime;
         Thread gameThread = null;
 
         SurfaceHolder gameHolder;
@@ -56,15 +63,30 @@ public class GameCanvas extends Activity {
         Brick[] bricks = new Brick[100];
         int numBricks = 0;
 
+        // For sound FX
+        SoundPool soundPool;
+        int beep1ID = -1;
+        int beep2ID = -1;
+        int beep3ID = -1;
+        int loseLifeID = -1;
+        int explodeID = -1;
+
+        // The score
+        int score = 0;
+
+        // Lives
+        //  int lives = 3;
+
 
 
         int gameLevel;
-        int score = 0;
 
 
         int lives = 3;
 
-        int wallRows;
+        int noOfRows;
+        int noOfColms;
+        float targetScore;
         int levelTracker;
 
 
@@ -72,6 +94,7 @@ public class GameCanvas extends Activity {
         public DxBall(Context context){
             super(context);
             levelTracker=0;
+
 
             //init game level
             GAME_LEVEL_ONE=true;
@@ -104,15 +127,53 @@ public class GameCanvas extends Activity {
             this.ballSpeed=40;
             this.barSpeed=17;
 
-            this.wallRows=2;
+            //init no of rows in wall
+            this.noOfRows =2;
+            this.noOfColms =8;
+            //init target score
+            this.targetScore = this.noOfColms*this.noOfRows*10;
 
-            firstTime = true;
+            //   firstTime = true;
+
+            // Load the sounds
+
+            // This SoundPool is deprecated but don't worry
+            soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC,0);
+
+            try{
+                // Create objects of the 2 required classes
+                AssetManager assetManager = context.getAssets();
+                AssetFileDescriptor descriptor;
+
+                // Load our fx in memory ready for use
+                descriptor = assetManager.openFd("beep1.ogg");
+                beep1ID = soundPool.load(descriptor, 0);
+
+                descriptor = assetManager.openFd("beep2.ogg");
+                beep2ID = soundPool.load(descriptor, 0);
+
+                descriptor = assetManager.openFd("beep3.ogg");
+                beep3ID = soundPool.load(descriptor, 0);
+
+                descriptor = assetManager.openFd("loseLife.ogg");
+                loseLifeID = soundPool.load(descriptor, 0);
+
+                descriptor = assetManager.openFd("explode.ogg");
+                explodeID = soundPool.load(descriptor, 0);
+
+            }catch(IOException e){
+                // Print an error message to the console
+                Log.e("error", "failed to load sound files");
+            }
 
             makeBrickWall();
 
         }
         public void makeBrickWall(){
 
+
+            //  score = 0;
+            lives = 3;
 
             int brickWidth = xResulation / 8;
             int brickHeight = yResulation /12;
@@ -124,8 +185,8 @@ public class GameCanvas extends Activity {
             int collisionCounter=0;
 
             //set Bricks positions
-            for(int column = 0; column < 8; column ++ ){
-                for(int row = 0; row < this.wallRows; row ++ ){
+            for(int column = 0; column < this.noOfColms; column ++ ){
+                for(int row = 0; row < this.noOfRows; row ++ ){
                     if(column%2==0 && row%2==0){
                         type=0;
                         bricks[numBricks] = new Brick(row, column, brickWidth, brickHeight,type,0);
@@ -199,6 +260,7 @@ public class GameCanvas extends Activity {
                 canvas.drawText("Point: " + score , xResulation-200,50, paint);
                 canvas.drawText( " gameLevel: " + gameLevel, xResulation-600,50, paint);
                 canvas.drawText( " Lives: " + lives, xResulation-1000,50, paint);
+                canvas.drawText( " h speed: " + ball.getHoraizontalSpeed(), xResulation-1000,200, paint);
 
                 gameHolder.unlockCanvasAndPost(canvas);
             }
@@ -229,26 +291,31 @@ public class GameCanvas extends Activity {
                                 bricks[i].setType(0);
                                 bricks[i].setCollisionCounter();
                             }
+                            //make sound when ball collid with the bricks
+                            soundPool.play(explodeID, 1, 1, 0, 0, 1);
 
                             ball.setVerticalSpeed();
 
                         }
                     }
                 }
-
+                // Check for ball colliding with the bar and make sound
                 if(RectF.intersects(bar.getBar(),ball.getBall())) {
                     ball.setRandomXVelocity();
                     ball.setVerticalSpeed();
                     ball.stopVtclOverlape(bar.getBar().top - 2);
+                    soundPool.play(beep1ID, 1, 1, 0, 0, 1);
                 }
 
-
+                // Bounce the ball back when it hits the bottom of screen
+                // And deduct a life
                 if(ball.getBall().bottom > yResulation){
                     ball.setVerticalSpeed();
                     ball.stopVtclOverlape(yResulation - 2);
 
                     // Lose a life
                     lives --;
+                    soundPool.play(loseLifeID, 1, 1, 0, 0, 1);
                     //when total numBricks=0 then game finish
                     if(lives == 0){
                         isRunning = true;
@@ -261,53 +328,63 @@ public class GameCanvas extends Activity {
 
                 if(ball.getBall().top < 0){
                     ball.setVerticalSpeed();
-
                     ball.stopVtclOverlape(100);
+                    soundPool.play(beep2ID, 1, 1, 0, 0, 1);
                 }
 
                 // If the ball hits left wall bounce
                 if(ball.getBall().left < 0){
                     ball.reverseXVelocity();
                     ball.stopHOverlap(20);
+                    soundPool.play(beep3ID, 1, 1, 0, 0, 1);
                 }
 
                 // If the ball hits right wall bounce
                 if(ball.getBall().right > xResulation){
                     ball.reverseXVelocity();
                     ball.stopHOverlap(xResulation - 50);
+                    soundPool.play(beep3ID, 1, 1, 0, 0, 1);
                 }
 
                 //check game score and  go to the next level
-                if (score == 160 && GAME_LEVEL_ONE==true){
+                if (score == targetScore && GAME_LEVEL_ONE==true){
                     GAME_LEVEL_ONE=false;
                     GAME_LEVEL_TWO=true;
+                    ballSpeed= ballSpeed-5;
                     gameLevel=2;
-                    this.wallRows=3;
+                    this.noOfRows =3;
+                    targetScore = targetScore+(this.noOfRows*this.noOfColms)*10;
                     ball.reset(xResulation,yResulation);
                     bar.barPositionReset();
                     makeBrickWall();
                     // levelTracker=0;
                 }
-                else if (score == 400 && GAME_LEVEL_TWO==true){
+                else if (score == targetScore && GAME_LEVEL_TWO==true){
                     GAME_LEVEL_TWO=false;
                     GAME_LEVEL_THREE = true;
                     gameLevel=3;
-                    this.wallRows=4;
+                    ballSpeed = ballSpeed-5;
+                    this.noOfRows =4;
+                    targetScore = targetScore+(this.noOfRows*this.noOfColms)*10;
                     ball.reset(xResulation,yResulation);
                     bar.barPositionReset();
                     makeBrickWall();
 
                 }
-                else if (score == 720 && GAME_LEVEL_THREE==true){
+                else if (score == targetScore && GAME_LEVEL_THREE==true){
+                    //  finish();
+                    GAME_LEVEL_ONE = true;
                     GAME_LEVEL_TWO=false;
-                    GAME_LEVEL_THREE = true;
+                    GAME_LEVEL_THREE = false;
                     gameLevel=1;
-                    this.wallRows=2;
+                    score=0;
+                    ballSpeed = ballSpeed+10;
+                    this.noOfRows =2;
+                    targetScore = (this.noOfRows*this.noOfColms)*10;
                     ball.reset(xResulation,yResulation);
                     bar.barPositionReset();
                     makeBrickWall();
 
-                    // finish();
                 }
                 // }
 
