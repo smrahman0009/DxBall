4abf3635a3f5cb83ccfc138451d576f32fd462f0
Mushfikur Rahman <35715315+smrahman0009@users.noreply.github.com>
2018-10-05 22:15:43 +0600

    add three buttons

--- app/src/main/java/com/example/musfiq/dxball/MainActivity.java
+++ app/src/main/java/com/example/musfiq/dxball/MainActivity.java
@@ -1,28 +1,10 @@
 package com.example.musfiq.dxball;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.res.AssetFileDescriptor;
-import android.content.res.AssetManager;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Point;
-import android.graphics.RectF;
-import android.media.AudioManager;
-import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Display;
-import android.view.MotionEvent;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
-
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button);
+        button = findViewById(R.id.buttonStart);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
